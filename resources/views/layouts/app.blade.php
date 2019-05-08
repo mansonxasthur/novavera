9c98b9298f7937f409e993886edfbb9e1dbd43c7
Manson Xasthur <mansonx13@gmail.com>
2019-05-08 19:43:16 +0200

    improvments

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,6 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
 
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
@@ -20,98 +19,101 @@
     @stack('styles')
 </head>
 <body>
-    <v-app id="app" v-cloak>
-        <v-navigation-drawer
-                fixed
-                v-model="drawer"
-                app
-                class="hidden-sm-and-up"
-        >
-            <v-list dense>
-                <v-list-tile @click="">
-                    <v-list-tile-action>
-                        <v-icon>dashboard</v-icon>
-                    </v-list-tile-action>
-                    <v-list-tile-content>
-                        <v-list-tile-title>Dashboard</v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile>
-                <v-list-tile @click="">
-                    <v-list-tile-action>
-                        <v-icon>settings</v-icon>
-                    </v-list-tile-action>
-                    <v-list-tile-content>
-                        <v-list-tile-title>Settings</v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile>
-            </v-list>
-        </v-navigation-drawer>
-        <v-toolbar app scroll-off-screen clipped-left color="white">
-            <v-toolbar-side-icon @click.stop="drawer = !drawer" class="hidden-sm-and-up"></v-toolbar-side-icon>
-            <a href="/#app" style="width: 170px">
-                <img src="{{ asset('assets/logo.svg') }}" alt="">
-            </a>
-            <v-spacer></v-spacer>
+<v-app id="app" v-cloak>
+    <v-navigation-drawer
+            fixed
+            v-model="drawer"
+            app
+            class="hidden-sm-and-up"
+    >
+        <v-list dense>
+            <v-list-tile @click="">
+                <v-list-tile-action>
+                    <v-icon>dashboard</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                    <v-list-tile-title>Dashboard</v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile @click="">
+                <v-list-tile-action>
+                    <v-icon>settings</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                    <v-list-tile-title>Settings</v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
+        </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app scroll-off-screen clipped-left color="white">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer" class="hidden-sm-and-up"></v-toolbar-side-icon>
+        <a href="/#app" style="width: 170px">
+            <img src="{{ asset('assets/logo.svg') }}" alt="">
+        </a>
+        <v-spacer></v-spacer>
 
-            <v-toolbar-items class="hidden-sm-and-down">
-                <v-btn href="/#app" flat>Home</v-btn>
-                <v-menu :nudge-width="100"
-                        offset-y
-                        transition="slide-x-transition"
-                >
-                    <template v-slot:activator="{ on }">
-                        <v-btn flat v-on="on">
-                            <span>Real State Brokerage</span>
-                            <v-icon>arrow_drop_down</v-icon>
-                        </v-btn>
-                    </template>
-
-                    <v-list>
-                        <v-list-tile>
-                            <v-list-tile-title>Residential Projects</v-list-tile-title>
-                        </v-list-tile>
-                        <v-list-tile>
-                            <v-list-tile-title>
-                                <v-btn flat href="{{ route('developers.index') }}"></v-btn>
-                            </v-list-tile-title>
-                        </v-list-tile>
-                    </v-list>
-                </v-menu>
-
-                <v-btn flat>Citizenship & Residency</v-btn>
-                <v-btn flat>About</v-btn>
-                <v-btn flat>Contact</v-btn>
-                <v-btn flat>Become a Partner</v-btn>
-            </v-toolbar-items>
-        </v-toolbar>
-        <v-content>
-            @yield('content')
-        </v-content>
-
-            <v-footer
-                    height="auto"
-                    class="nova-primary-bg"
+        <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn href="/#app" flat>Home</v-btn>
+            <v-menu :nudge-width="100"
+                    offset-y
+                    transition="slide-x-transition"
             >
-                <v-layout
-                        justify-center
-                        row
-                        wrap
-                >
-                    <v-flex
-                            primary
-                            py-3
-                            text-xs-center
-                            white--text
-                            xs12
-                    >
-                        Copyright © 2019 Novavera<sup>®</sup> - All Rights Reserved.
-                    </v-flex>
-                </v-layout>
-            </v-footer>
+                <template v-slot:activator="{ on }">
+                    <v-btn flat v-on="on">
+                        <span>Real State Brokerage</span>
+                        <v-icon>arrow_drop_down</v-icon>
+                    </v-btn>
+                </template>
 
-    </v-app>
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    @stack('scripts')
+                <v-list>
+                    <v-list-tile>
+                        <v-list-tile-title>Residential Projects</v-list-tile-title>
+                    </v-list-tile>
+
+                    <a href="{{ route('developers.index') }}" style="text-decoration: none; color: inherit">
+                        <v-list-tile>
+                            <v-list-tile-title>Developers</v-list-tile-title>
+                        </v-list-tile>
+                    </a>
+
+
+                </v-list>
+            </v-menu>
+
+            <v-btn flat>Citizenship & Residency</v-btn>
+            <v-btn flat>About</v-btn>
+            <v-btn flat>Contact</v-btn>
+            <v-btn flat>Become a Partner</v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+        @yield('content')
+    </v-content>
+
+    <v-footer
+            height="auto"
+            class="nova-primary-bg"
+    >
+        <v-layout
+                justify-center
+                row
+                wrap
+        >
+            <v-flex
+                    primary
+                    py-3
+                    text-xs-center
+                    white--text
+                    xs12
+            >
+                Copyright © 2019 Novavera<sup>®</sup> - All Rights Reserved.
+            </v-flex>
+        </v-layout>
+    </v-footer>
+
+</v-app>
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}" defer></script>
+@stack('scripts')
 </body>
 </html>
