9489e6e7b8db83233c0e352c36f4f3a5efcd2063
Manson Xasthur <mansonx13@gmail.com>
2019-07-05 12:33:33 +0200

    new updates

--- resources/views/page/show.blade.php
+++ resources/views/page/show.blade.php
@@ -10,10 +10,29 @@
     @endcomponent
 @endpush
 
+@push('bootstrap')
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+@endpush
+
 @push('title')
     @component('partials.title')
         {{ $page->title }}
     @endcomponent
+@endpush
+
+@push('styles')
+    <style>
+        .v-menu__content.theme--light.v-menu__content--fixed.menuable__content__active.citizenship-list {
+            padding-left: 12px;
+            padding-right: 12px;
+        }
+
+        a.text-capitalize.font-weight-bold.subheading.v-btn.v-btn--flat.theme--light {
+            text-decoration: none;
+        }
+    </style>
 @endpush
 
 @if($page->style)
